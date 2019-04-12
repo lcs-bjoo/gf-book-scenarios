72303be4d12b84359c69d95cc3984ff41b20dfd1
Brandon Joo <bjoo@lcs.on.ca>
2019-04-12 14:54:05 -0400

    Made the sound play

--- chapter06/piano-1/Key.java
+++ chapter06/piano-1/Key.java
@@ -1,35 +1,47 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 
 public class Key extends Actor
-
-
 {
+    //tracks whether is "piano key down" image has already been set or not.
     private boolean isDown;
     /**
      * Create a new key.
-     * Contructor - it runs once when a key object is created
+     * Constructor
      */
     public Key()
     {
+        //Key begins in the "up" position
         isDown = false;
+        
     }
+
     /**
      * Do the action for this key.
      */
     public void act()
     {
-        //when key image is not already showing"down"
-        //and the g key on the keyboard has been pressed
-        if ( isDown = false && Greenfoot.isKeyDown("g") )
+        //when the key image is not already showing "down"
+        //AND
+        //the "g"key on the keyboard has been pressed
+        if (!isDown && Greenfoot.isKeyDown("g"))
         {
-           setImage("white-key-down.png"); 
-           isDown = true;
+            setImage("white-key-down.png");
+            play();
+            isDown = true;
         }
-        if ( isDown == true && Greenfoot.KeyDown("g") == false )
+        if (isDown == true && !Greenfoot.isKeyDown("g"))
         {
             setImage("white-key.png");
             isDown = false;
         }
     }
+    /**
+     * Play the note of this key
+     */
+    public void play()
+    {
+        Greenfoot.playSound("3a.wav");
+    }
 }
+
 
--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -36,10 +36,10 @@ public class Piano extends World
         //Every second, say hello to the next person in the list
         // Use a Boolean and to check both conditions
         // The showText statement only runs when both conditions are true
-        if ((frames % 60 == 0) && (frames / 60 < 60))
+        //if ((frames % 60 == 0) && (frames / 60 < 60))
         {
             
-            showText("Hello " + studentNames[frames / 60], 400, 170);
+            //showText("Hello " + studentNames[frames / 60], 400, 170);
         }
         // keep track of frames
         frames += 1;
